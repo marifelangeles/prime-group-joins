b72bd6372105348eb0037e919ada90d357974614
Marifel Angeles <mmarifel@gmail.com>
2019-03-05 11:05:46 -0600

    answer Qs 1-4

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,12 +1,25 @@
 -- BASE GOALS
 
 -- Get all customers and their addresses.
+SELECT "first_name", "last_name", "street", "city", "state", "zip", "address_type" FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id";
 
 -- Get all orders and their line items (orders, quantity and product).
+SELECT * FROM "orders"
+JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
+JOIN "products" ON "products"."id" = "line_items"."product_id";
 
 -- Which warehouses have cheetos?
+SELECT * FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'cheetos';
 
 -- Which warehouses have diet pepsi?
+SELECT * FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
 
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
