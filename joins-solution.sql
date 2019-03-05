54bca5d270adf1c319b979368314a05fed98f09b
Marifel Angeles <mmarifel@gmail.com>
2019-03-05 15:44:33 -0600

    answer Qs 5-8

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,37 +1,50 @@
 -- BASE GOALS
 
--- Get all customers and their addresses.
+-- 1. Get all customers and their addresses.
 SELECT "first_name", "last_name", "street", "city", "state", "zip", "address_type" FROM "customers"
 JOIN "addresses" ON "customers"."id" = "addresses"."customer_id";
 
--- Get all orders and their line items (orders, quantity and product).
+-- 2. Get all orders and their line items (orders, quantity and product).
 SELECT * FROM "orders"
 JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
 JOIN "products" ON "products"."id" = "line_items"."product_id";
 
--- Which warehouses have cheetos?
+-- 3. Which warehouses have cheetos?
 SELECT * FROM "warehouse"
 JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
 JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
 WHERE "products"."description" = 'cheetos';
 
--- Which warehouses have diet pepsi?
+-- 4. Which warehouses have diet pepsi?
 SELECT * FROM "warehouse"
 JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
 JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
 WHERE "products"."description" = 'diet pepsi';
 
--- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT "first_name", "last_name", COUNT("order_date") FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id"
+JOIN "orders" ON "orders"."address_id" = "addresses"."id"
+GROUP BY "first_name", "last_name";
 
--- How many customers do we have?
+-- 6. How many customers do we have?
+SELECT COUNT(*) AS "customer_count" FROM "customers";
 
--- How many products do we carry?
+-- 7. How many products do we carry?
+SELECT COUNT("products"."id") AS "product_count" FROM "products";
 
--- What is the total available on-hand quantity of diet pepsi?
-
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("warehouse_product"."on_hand") AS "diet_pepsi_sum" FROM "products"
+JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
 
 -- STRETCH GOALS
 
--- How much was the total cost for each order?
--- How much has each customer spent in total?
--- How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
+-- 9. How much was the total cost for each order?
+SELECT "order_date", SUM("products"."unit_price") FROM "orders"
+JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
+JOIN "products" ON "products"."id" = "line_items"."product_id"
+GROUP BY "order_date";
+
+-- 10. How much has each customer spent in total?
+-- 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
